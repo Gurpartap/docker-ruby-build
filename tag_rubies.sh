bdea816d469a75a3b5be151f5ebed8cd84967502
Gurpartap Singh <contact@gurpartap.com>
2014-08-31 04:28:12 +0530

    Script for auto tagging Dockerfile for rubies

---
+++ tag_rubies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Usage: tag_rubies.sh
+#
+# - push master
+# - read available versions from https://github.com/sstephenson/ruby-build/tree/master/share/ruby-build
+# - for each version, populate the placeholder in Dockerfile and (re)tag the repo
+# - push tags to Github
+#
+
+git commit -m "Committing master Dockerfile ($(date '+%Y%m%d'))" Dockerfile
+git push origin master
+
+rubies=$(curl --silent -–show-error https://api.github.com/repos/sstephenson/ruby-build/contents/share/ruby-build | awk '/"name":/ { print $2 }' | tr -d '",')
+
+for ruby_tag in rubies
+do
+  sed -i -e "s/\${ruby_tag}/${ruby_tag}/g" Dockerfile
+  git commit -m "Merging changes on master to ruby-${ruby_tag}"
+  git tag -d "${ruby_tag}"
+  git tag "${ruby_tag}"
+  git checkout -q master
+done
+
+git push --tags
